0ef8befd9c30884a81a733c217babe88a434fb47
rclepf <rclepf@gmail.com>
2022-07-26 20:37:37 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using RPGDIO.src.entities;
+
+class Program
+{
+    static void Main(string[] args)
+    {   
+        Arus hero = new Arus();
+        hero.Name = "Arus";
+        hero.level = 2;
+        hero.HeroType = "Knight";
+
+        Console.Write(" ");
+    }
+}
---
+++ src/entities/Arus.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RPGDIO.src.entities
+{
+    public class Arus
+    {
+        public string Name;
+        public int level;
+        public string HeroType;
+
+    }
+}
